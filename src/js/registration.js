c86faf676d7f692ce741b4ddaf38145085f76a54
YellowCatt <32128092+YellowCatt@users.noreply.github.com>
2018-05-05 18:14:37 +0300

    Add files via upload

---
+++ src/js/registration.js
@@ -0,0 +1,35 @@
+function registration(event){
+  var email = document.getElementById('email');
+  var pass = document.getElementById('password');
+  var passCheck = document.getElementById('password_repeat');
+  var agreement = document.getElementById('check_agreement');  
+  var emailCheck = (/[^\s@]+@[^\s@]+\.[^\s@]+/.test(email.value));
+
+  event.preventDefault();
+  if ((email.value == "") || (pass.value == "") || (passCheck.value == "")){
+    alert("Введите данные"); 
+    return;
+    } else {
+      if ((email.value.length < 5) || (!emailCheck)) {
+          alert("Email введен неправильно");
+          return;
+      }
+      if ((pass.value.length < 6) || (pass == "") || (passCheck == "")) {
+          alert("Длина пароля должна быть не меньше 6-ти символов");
+          return;
+      }
+      if (pass.value != passCheck.value) {
+          alert("Пароли не совпадают");
+          return;
+      }
+      if (!agreement.checked){
+          alert("Примите условия пользовательского соглашение");
+          return;
+      }
+      alert("Регистрация успешна!");
+      }
+  }
+  
+  window.onload = function(){
+    document.getElementById("registration").addEventListener("submit", registration);
+  };
